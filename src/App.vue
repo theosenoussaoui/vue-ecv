9758caaef210cac344b67329165239316219a2b1
tsenoussaoui <tsenoussaoui@wedigital.garden>
2022-02-22 00:16:58 +0100

    feat: adding tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TailwindExample msg="voici avec tailwind" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TailwindExample from "./components/TailwindExample.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TailwindExample,
+  },
+};
 </script>
 
 <style>
